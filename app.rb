1ff470dc2ad8173c89d480f3b68cc155ca8b5bea
jennifer tracy <jenniferetracy@gmail.com>
2021-01-10 11:36:29 -0500

    commit tree b1aeecb1da0e6938c0f6e3db6c5b948c4ffa1646

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Jen"
+    end
+
+    get '/hometown' do 
+        "My hometown is Alpharetta, GA"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'A Million Dreams'"
+    end
 end
